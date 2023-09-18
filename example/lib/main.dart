e8827e512285e79d8f22e2304505b676fe1b3a84
frybitsinc <frybitsinc@gmail.com>
2023-09-18 16:31:59 +0900

    add save, export buttons to example for testing

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,51 +19,116 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('PencilKit Example'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.palette),
-              onPressed: () => controller.show(),
-            ),
-            IconButton(
-              icon: const Icon(Icons.close),
-              onPressed: () => controller.hide(),
-            ),
-            IconButton(
-              icon: const Icon(Icons.undo),
-              onPressed: () => controller.undo(),
-            ),
-            IconButton(
-              icon: const Icon(Icons.redo),
-              onPressed: () => controller.redo(),
-            ),
-            IconButton(
-              icon: const Icon(Icons.refresh),
-              onPressed: () => controller.clear(),
-            ),
-          ],
-        ),
-        body: PencilKit(
-          onPencilKitViewCreated: (controller) => this.controller = controller,
-          alwaysBounceVertical: false,
-          alwaysBounceHorizontal: true,
-          isRulerActive: false,
-          drawingPolicy: PencilKitIos14DrawingPolicy.anyInput,
-          onToolPickerVisibilityChanged: (isVisible) {
-            if (kDebugMode) {
-              print('isToolPickerVisible $isVisible');
-            }
-          },
-          onRulerActiveChanged: (isRulerActive) {
-            if (kDebugMode) {
-              print('isRulerActive $isRulerActive');
-            }
-          },
-          backgroundColor: Colors.blue.withOpacity(0.1),
-          isOpaque: false,
-        ),
+      home: SafeArea(
+        child: Scaffold(
+            body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                alignment: Alignment.center,
+                color: Colors.green.withOpacity(0.25),
+                width: MediaQuery.of(context).size.width,
+                height: 42,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'PencilKit Example',
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(
+                            Icons.palette,
+                            color: Colors.black,
+                          ),
+                          onPressed: () => controller.show(),
+                        ),
+                        IconButton(
+                          icon: const Icon(
+                            Icons.close,
+                            color: Colors.black,
+                          ),
+                          onPressed: () => controller.hide(),
+                        ),
+                        IconButton(
+                          icon: const Icon(
+                            Icons.undo,
+                            color: Colors.black,
+                          ),
+                          onPressed: () => controller.undo(),
+                        ),
+                        IconButton(
+                          icon: const Icon(
+                            Icons.redo,
+                            color: Colors.black,
+                          ),
+                          onPressed: () => controller.redo(),
+                        ),
+                        IconButton(
+                          icon: const Icon(
+                            Icons.refresh,
+                            color: Colors.black,
+                          ),
+                          onPressed: () => controller.clear(),
+                        ),
+                      ],
+                    ),
+                    // add buttons for new feature - save, export
+                    Row(
+                      children: [
+                        // save button
+                        IconButton(
+                          icon: const Icon(
+                            Icons.save,
+                            color: Colors.black,
+                          ),
+                          onPressed: () => controller.clear(),
+                        ),
+                        // export button
+                        IconButton(
+                          icon: const Icon(
+                            Icons.download,
+                            color: Colors.black,
+                          ),
+                          onPressed: () => controller.clear(),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: PencilKit(
+                  onPencilKitViewCreated: (controller) => this.controller = controller,
+                  alwaysBounceVertical: false,
+                  alwaysBounceHorizontal: true,
+                  isRulerActive: false,
+                  drawingPolicy: PencilKitIos14DrawingPolicy.anyInput,
+                  onToolPickerVisibilityChanged: (isVisible) {
+                    if (kDebugMode) {
+                      print('isToolPickerVisible $isVisible');
+                    }
+                  },
+                  onRulerActiveChanged: (isRulerActive) {
+                    if (kDebugMode) {
+                      print('isRulerActive $isRulerActive');
+                    }
+                  },
+                  backgroundColor: Colors.white,
+                  isOpaque: false,
+                ),
+              ),
+            ],
+          ),
+        )),
       ),
     );
   }
